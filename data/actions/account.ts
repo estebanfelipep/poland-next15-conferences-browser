073c1a6b63ed348b828bc136b1af7f4e420557d3
Aurora Scharff <aurora.sofie@gmail.com>
2025-05-31 19:21:20 +0200

    Delete toast

--- data/actions/account.ts
+++ data/actions/account.ts
@@ -1,9 +1,7 @@
 'use server';
 
 import { cookies } from 'next/headers';
-import { toast } from '../utils/toast';
 
 export async function setCurrentAccount(accountId: string) {
   (await cookies()).set('selectedAccountId', accountId);
-  await toast.success('Account changed successfully!');
 }
--- types/toast.ts
+++
@@ -1,7 +0,0 @@
-export type ToastType = 'success' | 'error';
-
-export type Toast = {
-  id: string;
-  message: string;
-  type: ToastType;
-};
