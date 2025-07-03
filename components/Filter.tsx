08824f764a114957b61c293276104542b1cd7f18
Aurora Scharff <aurora.sofie@gmail.com>
2025-07-03 23:11:28 +0200

    update css

--- components/Filter.tsx
+++ components/Filter.tsx
@@ -18,7 +18,7 @@ export default function Filter({ filterOptions, filters }: Props) {
 
   return (
     <div className="flex items-center justify-between gap-4">
-      <div className="grid w-full grid-cols-2 gap-4 md:grid-cols-4 md:gap-6">
+      <div className="grid w-full grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-4 md:gap-6">
         <RouterSelect
           name="year"
           label="Year"
