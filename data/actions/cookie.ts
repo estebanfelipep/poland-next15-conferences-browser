e19c9afd0e91b27b0bd91802e1bf8b5ea8d8de58
Aurora Scharff <aurora.sofie@gmail.com>
2025-07-05 11:09:28 +0200

    Dont redirect on cookie

--- data/actions/cookie.ts
+++ data/actions/cookie.ts
@@ -1,7 +1,6 @@
 'use server';
 
 import { cookies } from 'next/headers';
-import { redirect } from 'next/navigation';
 import { slow } from '@/utils/slow';
 
 export async function someRandomServerFunction(conference?: string, year?: string) {
@@ -12,7 +11,6 @@ export async function someRandomServerFunction(conference?: string, year?: strin
     return;
   }
   (await cookies()).set('easterEgg', 'true');
-  redirect('/');
 }
 
 export async function resetEasterEgg() {
