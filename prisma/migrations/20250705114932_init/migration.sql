b7c56e1982a43b5eb71652ac01bb4826e16918b5
Aurora Scharff <aurora.sofie@gmail.com>
2025-07-05 13:49:46 +0200

    Update schema and migraations

--- prisma/migrations/20250705114932_init/migration.sql
+++ prisma/migrations/20250705114932_init/migration.sql
@@ -1,7 +1,7 @@
 -- CreateTable
 CREATE TABLE "Talk" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "title" TEXT NOT NULL,
     "speaker" TEXT NOT NULL,
     "description" TEXT,
@@ -10,5 +10,7 @@ CREATE TABLE "Talk" (
     "conference" TEXT NOT NULL,
     "duration" INTEGER,
     "videoUrl" TEXT,
-    "slides" TEXT
+    "slides" TEXT,
+
+    CONSTRAINT "Talk_pkey" PRIMARY KEY ("id")
 );
