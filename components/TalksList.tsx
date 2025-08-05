ad2b2ee7a8a7b17faff829b9718d32aa5e3b21dc
Aurora Scharff <aurora.sofie@gmail.com>
2025-08-05 18:36:54 +0200

    fix: hydration error in view transition

--- components/TalksList.tsx
+++ components/TalksList.tsx
@@ -46,28 +46,30 @@ export default function TalksList({ talksPromise, search }: Props) {
   });
 
   return (
-    <ViewTransition>
+    <>
       <div
         className={`grid gap-6 ${expandedTalkId ? 'sm:grid-cols-1 lg:grid-cols-2' : 'sm:grid-cols-2 lg:grid-cols-3'}`}
       >
-        {filteredTalks.map(talk => {
-          const isExpanded = expandedTalkId === talk.id;
-          return (
-            <div key={talk.id} className={isExpanded ? 'sm:col-span-1 lg:col-span-2' : ''}>
-              <ViewTransition name={`talk-${talk.id}`}>
-                <TalkItem
-                  talk={talk}
-                  isExpanded={isExpanded}
-                  onToggleExpand={() => {
-                    startTransition(() => {
-                      setExpandedTalkId(isExpanded ? null : talk.id);
-                    });
-                  }}
-                />
-              </ViewTransition>
-            </div>
-          );
-        })}
+        <ViewTransition>
+          {filteredTalks.map(talk => {
+            const isExpanded = expandedTalkId === talk.id;
+            return (
+              <div key={talk.id} className={isExpanded ? 'sm:col-span-1 lg:col-span-2' : ''}>
+                <ViewTransition name={`talk-${talk.id}`}>
+                  <TalkItem
+                    talk={talk}
+                    isExpanded={isExpanded}
+                    onToggleExpand={() => {
+                      startTransition(() => {
+                        setExpandedTalkId(isExpanded ? null : talk.id);
+                      });
+                    }}
+                  />
+                </ViewTransition>
+              </div>
+            );
+          })}
+        </ViewTransition>
       </div>
       {filteredTalks.length === 0 && (
         <div className="py-16 text-center text-gray-500 dark:text-gray-400">
@@ -79,7 +81,7 @@ export default function TalksList({ talksPromise, search }: Props) {
       <div className="flex h-5 justify-center pt-5" ref={ref}>
         {loading && <Spinner />}
       </div>
-    </ViewTransition>
+    </>
   );
 }
 
