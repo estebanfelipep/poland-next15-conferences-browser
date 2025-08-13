73786072e0c194e3623570157088773a1fc3553e
Aurora Scharff <aurora.sofie@gmail.com>
2025-08-13 19:56:36 +0200

    Make filters responsive for mobile

--- components/Filters.tsx
+++ components/Filters.tsx
@@ -12,7 +12,7 @@ export default function Filters({ filterOptions, filters }: Props) {
 
   return (
     <div className="flex items-center justify-between gap-4">
-      <div className="grid w-full grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-4 md:gap-6">
+      <div className="grid w-full grid-cols-2 gap-4 md:grid-cols-4 md:gap-6">
         <AsyncSelect
           name="year"
           label="Year"
